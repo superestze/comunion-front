a62104cf8581e4bc6646be496fb7cbe77440d825
erguotou <erguotou525@gmail.com>
2021-06-20 09:56:38 +0800

    feat(web): ✨ add web content

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,10 @@
+
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
