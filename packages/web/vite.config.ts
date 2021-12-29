0611078fec18d5ba5c852a27aadf391ab69470a7
erguotou <erguotou525@gmail.com>
2021-12-29 14:15:06 +0800

    fix: 🐛 Fix wallet-collect build

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,12 +5,17 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import pages from 'vite-plugin-pages'
 import WindiCSS from 'vite-plugin-windicss'
+// import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '/src')
+      '@': path.resolve(__dirname, '/src'),
+      '@walletconnect/web3-provider': path.resolve(
+        __dirname,
+        '../../node_modules/@walletconnect/web3-provider/dist/umd/index.min.js'
+      )
     }
   },
   server: {
@@ -38,5 +43,48 @@ export default defineConfig({
       nuxtStyle: true
     })
     // polyfillNode()
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        // manualChunks: {
+        //   wallet: ['@walletconnect/web3-provider'],
+        //   ethers: ['ethers'],
+        //   vue: ['vue', 'vue-router', 'pinia'],
+        //   tools: ['axios', 'buffer', 'events', 'util']
+        // }
+        manualChunks(id) {
+          const chunkMap = {
+            '@walletconnect/web3-provider': 'wallet',
+            '@ethersproject': 'ethers',
+            ethers: 'ethers',
+            vue: 'vue',
+            'vue-router': 'vue',
+            pinia: 'vue',
+            axios: 'tools',
+            buffer: 'tools',
+            events: 'tools',
+            util: 'tools'
+          }
+          const splited = id.split('node_modules' + path.sep)
+          if (splited.length > 1) {
+            let pkgName
+            if (splited[1].startsWith('@')) {
+              pkgName = splited[1].split('/').slice(0, 2).join('/')
+            } else {
+              pkgName = splited[1].split('/')[0]
+            }
+            if (
+              Object.keys(chunkMap).includes(pkgName) ||
+              (pkgName.startsWith('@') &&
+                Object.keys(chunkMap).find(key => pkgName.startsWith(key)))
+            ) {
+              return chunkMap[pkgName]
+            }
+            return 'bundle'
+          }
+        }
+      }
+    }
+  }
 })
