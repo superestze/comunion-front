1ec307d8b1774890d244fc37965e0b7daee1dbf4
wujiu2020 <wujiu2020@gmail.com>
2022-01-29 23:07:23 +0800

    feat: ✨ delete styleProvider

--- packages/components/src/UTag/index.tsx
+++ packages/components/src/UTag/index.tsx
@@ -1,14 +1,9 @@
 import { defineComponent } from 'vue'
 import './index.css'
-import { StyleProvider } from '..'
 
 export default defineComponent({
   name: 'UTag',
   setup(props, { slots }) {
-    return () => (
-      <StyleProvider>
-        <div class="u-tag">{slots.default?.()}</div>
-      </StyleProvider>
-    )
+    return () => <div class="u-tag">{slots.default?.()}</div>
   }
 })
--- packages/components/src/UViewMore/index.tsx
+++ packages/components/src/UViewMore/index.tsx
@@ -1,18 +1,15 @@
 import { defineComponent } from 'vue'
 import { ArrowRightOutlined } from '@comunion/icons'
-import { StyleProvider } from '..'
 import './index.css'
 
 export default defineComponent({
   name: 'UViewMore',
   setup(props, { slots }) {
     return () => (
-      <StyleProvider>
-        <div class="u-view-more flex">
-          <div>{slots.default?.() ?? 'View more'}</div>
-          <ArrowRightOutlined class="u-view-more-icon" />
-        </div>
-      </StyleProvider>
+      <div class="u-view-more flex">
+        <div>{slots.default?.() ?? 'View more'}</div>
+        <ArrowRightOutlined class="u-view-more-icon" />
+      </div>
     )
   }
 })
