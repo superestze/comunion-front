feae0af0af3cef60da98b27be112afde2aade846
erguotou <erguotou525@gmail.com>
2022-03-07 15:02:22 +0800

    feat: ✨ temp umarkdown

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'unused-imports/no-unused-imports': 'warn',
     'import/order': [
       'error',
