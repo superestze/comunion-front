9f614171ecb38b98b6f11c01c00ad3d7019bee79
zehui <lizh285@midea.com>
2022-04-05 16:33:36 +0800

    feat: ✨ Add create startup button in my startups card in dashboard

--- packages/web/src/pages/dashboard/components/Startups.tsx
+++ packages/web/src/pages/dashboard/components/Startups.tsx
@@ -1,12 +1,26 @@
 import { UCard, UDeveloping, UTabPane, UTabs } from '@comunion/components'
+import { PlusOutlined } from '@comunion/icons'
 import { defineComponent, onMounted, ref } from 'vue'
 import StartupCard from './StartupCard'
+import CreateStartupBlock from '@/blocks/Startup/Create'
 import { services } from '@/services'
 
 const Startups = defineComponent({
   name: 'Startups',
   setup(prop, ctx) {
     const myCreatedStartups = ref([])
+    const slots = {
+      'header-extra': () => (
+        <CreateStartupBlock
+          trigger={
+            <span class="flex flex-row items-center text-primary font-opensans font-700 text-[14px] tracking-2px cursor-pointer">
+              <PlusOutlined class="h-4 mr-3 w-4" />
+              CREATE NEW{' '}
+            </span>
+          }
+        />
+      )
+    }
     const getCreatedStartups = async (page = 1, pageSize = 4) => {
       const { error, data } = await services['startup@startup-list-me']({
         limit: pageSize,
@@ -22,7 +36,12 @@ const Startups = defineComponent({
     })
 
     return () => (
-      <UCard title="MY COMEUPS" size="small" class="p-10 font-700 font-4 leading-6 tracking-2px">
+      <UCard
+        title="MY STARTUPS"
+        size="small"
+        class="p-10 font-700 font-4 leading-6 tracking-2px"
+        v-slots={slots}
+      >
         <UTabs>
           <UTabPane name="WHAT I PARTICIPATED" tab="WHAT I PARTICIPATED">
             <UDeveloping />
