b51821dee89d17f8ec1a788ca32a311a9fd4ca4b
erguotou <erguotou525@gmail.com>
2022-04-13 10:21:13 +0800

    fix: 🐛 format issue fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'stylelint-config-css-modules',
     'stylelint-config-rational-order',
     'stylelint-no-unsupported-browser-features',
-    'stylelint-config-prettier',
+    'stylelint-config-prettier'
   ],
   plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
