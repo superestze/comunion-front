a1e50ffa918f12ab60c9d0c631541edc91712b21
qihianan <qhn525426@163.com>
2022-04-26 19:59:17 +0800

    feat: ✨  Members style center modification

--- packages/web/src/pages/startups/components/TeamMembers.tsx
+++ packages/web/src/pages/startups/components/TeamMembers.tsx
@@ -7,14 +7,14 @@ const UTeamMembers = defineComponent({
   setup(props, ctx) {
     return () => (
       <div class={styles.team_members}>
-        <div class="flex">
+        <div class="flex align-center">
           <span class={styles.members_svg}>
             <TeamOutlined class="text-primary m-0.5-0.5" />
           </span>
           <span class="pl-3 text-20px font-bold pt-1">
             <i class={styles.members_span_i}>1,342</i>
           </span>
-          <span class="pl-3 pt-1">Members</span>
+          <span class="m-auto ml-3">Members</span>
         </div>
       </div>
     )
