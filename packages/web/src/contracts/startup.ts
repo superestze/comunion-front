92ba39ee7217c09c02cbe7f369be1f334818337a
zehui <lizh285@midea.com>
2022-05-03 22:48:19 +0800

    fix: 🐛 update startup address for avalanch testnet

--- packages/web/src/contracts/startup.ts
+++ packages/web/src/contracts/startup.ts
@@ -2,7 +2,7 @@ import { Contract } from 'ethers'
 import { wrapTransaction } from './share'
 import { useWalletStore } from '@/stores'
 
-const address = '0xF98A7F9E86DCE7298F3be4778ACd692D649c5228'
+const address = '0x7E94572BCc67B6eDa93DBa0493b681dC0ae9E964'
 const abi =
   '[{"inputs":[{"components":[{"internalType":"string","name":"name","type":"string"},{"internalType":"enum Startup.Mode","name":"mode","type":"uint8"},{"internalType":"string","name":"logo","type":"string"},{"internalType":"string","name":"mission","type":"string"},{"internalType":"string","name":"overview","type":"string"},{"internalType":"bool","name":"isValidate","type":"bool"}],"internalType":"struct Startup.Profile","name":"p","type":"tuple"}],"name":"newStartup","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"startups","outputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"enum Startup.Mode","name":"mode","type":"uint8"},{"internalType":"string","name":"logo","type":"string"},{"internalType":"string","name":"mission","type":"string"},{"internalType":"string","name":"overview","type":"string"},{"internalType":"bool","name":"isValidate","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address payable","name":"receiver","type":"address"}],"name":"suicide0","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
 
