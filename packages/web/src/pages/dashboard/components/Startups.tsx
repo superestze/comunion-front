745cc29897db22cb26cb351a14d3ba7b20af5d34
zehui <lizh285@midea.com>
2022-05-05 19:25:39 +0800

    fix: 🐛 change order of startup

--- packages/web/src/pages/dashboard/components/Startups.tsx
+++ packages/web/src/pages/dashboard/components/Startups.tsx
@@ -65,7 +65,11 @@ const Startups = defineComponent({
       >
         <CreateStartupBlock ref={createRef} />
         <UTabs>
-          {/* TODO @zehui if PARTICIPATED list finished, PARTICIPATED list should before "created by me" startup list */}
+          <UTabPane name="PARTICIPATED" tab="PARTICIPATED">
+            <UDeveloping>
+              <EmptyFilled class="mt-34" />
+            </UDeveloping>
+          </UTabPane>
           <UTabPane name="CREATED BY ME" tab="CREATED BY ME" class="h-112">
             <UScrollList
               triggered={pagination.loading}
@@ -80,11 +84,6 @@ const Startups = defineComponent({
                 <UDeveloping />
               )}
             </UScrollList>
-          </UTabPane>
-          <UTabPane name="PARTICIPATED" tab="PARTICIPATED">
-            <UDeveloping>
-              <EmptyFilled class="mt-34" />
-            </UDeveloping>
           </UTabPane>
         </UTabs>
       </UCard>
