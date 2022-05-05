5f5bf891b2225560320858bd894a68aa84db9bc9
zehui <lizh285@midea.com>
2022-05-05 19:30:26 +0800

    fix: 🐛 update "To be develop" to "To be developed"

--- packages/components/src/UDeveloping/Developing.tsx
+++ packages/components/src/UDeveloping/Developing.tsx
@@ -7,7 +7,7 @@ export const UDeveloping = defineComponent({
     return () => (
       <div class="u-developing">
         <div class="u-developing__img">{slots.default?.()}</div>
-        <div class="u-developing__text">To be develop</div>
+        <div class="u-developing__text">To be developed</div>
       </div>
     )
   }
