18cece199cc0c20e8a07a51f1a212da03781c6f8
superestze <lizh285@midea.com>
2022-05-06 22:46:06 +0800

    fix: 🐛 fix show issues

--- packages/web/src/pages/dashboard/index.tsx
+++ packages/web/src/pages/dashboard/index.tsx
@@ -85,7 +85,7 @@ const DashboardPage = defineComponent({
               <div class="mt-2">
                 {myInfo.value?.map(info => {
                   return (
-                    <div class="flex mb-3 mt-3 break-words">
+                    <div key={info.label} class="flex mb-3 mt-3 break-words">
                       <div class="u-label2 uppercase text-grey3 w-50">{info.label}</div>
                       <div class="u-body2 text-grey1 flex-1 break-all max-h-37 overflow-auto">
                         {info.value}
