568edeeed8beedad54938e017a9545a6dac8a4fc
geekknight <geekknight@163.com>
2022-05-10 00:54:28 +0800

    fix: 🐛 修复mode问题

--- packages/web/src/constants/startup.ts
+++ packages/web/src/constants/startup.ts
@@ -2,7 +2,7 @@
  * Startup types
  */
 export const STARTUP_TYPES = ['ESG', 'NGO', 'DAO', 'COM'] as const
-export type StartupTypesType = typeof STARTUP_TYPES[number] | undefined
+export type StartupTypesType = typeof STARTUP_TYPES[number] & undefined
 export const STARTUP_TYPES_COLOR_MAP = {
   ESG: '#947DFF',
   NGO: '#FC8181',
@@ -21,8 +21,5 @@ export function getStartupTypeFromNumber(mode: number) {
  * @return 0:NONE,1:ESG,2:NGO,3:DAO,4:COM
  */
 export function getStartupNumberFromType(type: StartupTypesType) {
-  if (type === undefined) {
-    return 0
-  }
   return STARTUP_TYPES.indexOf(type) + 1
 }
