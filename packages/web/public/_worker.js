183daa6bc1b1474a0c0e50c600122102a193f222
erguotou <erguotou525@gmail.com>
2022-05-26 08:04:26 +0800

    feat: ✨ update worker

--- packages/web/public/_worker.js
+++ packages/web/public/_worker.js
@@ -1,19 +1,14 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-/**
- * Many more examples available at:
- *   https://developers.cloudflare.com/workers/examples
- * @param {Request} request
- * @returns {Promise<Response>}
- */
-async function handleRequest(request) {
-  const url = new URL(request.url)
-
-  if (url.pathname.startsWith('/api')) {
-    return fetch(url.href.replace(url.host, 'd.comunion.io'), request)
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    if (url.pathname.startsWith('/api/')) {
+      return fetch(
+        url.href.replace(url.host, env.Proxy.get('API_PROXY_HOST') || 'd.comunion.io'),
+        request
+      )
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request)
   }
-
-  return fetch(request)
 }
