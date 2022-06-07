ceb80e51987e133e0d231f424d148671d7c00dc3
geekknight <geekknight@163.com>
2022-06-07 22:13:26 +0800

    fix: 🐛 perf socialGroup

--- packages/web/src/pages/startup/components/SocialGroup.tsx
+++ packages/web/src/pages/startup/components/SocialGroup.tsx
@@ -13,63 +13,47 @@ export const SocialGroup = defineComponent({
   name: 'socialGroup',
   props: ['website', 'discord', 'telegram', 'twitter', 'docs'],
   setup(props) {
-    const website = computed(() => props.website)
-    const discord = computed(() => props.discord)
-    const telegram = computed(() => props.telegram)
-    const twitter = computed(() => props.twitter)
-    const docs = computed(() => props.docs)
+    const socialData = computed(() => [
+      {
+        component: WebsiteFilled,
+        value: props.website
+      },
+      {
+        component: DiscordFilled,
+        value: props.discord
+      },
+      {
+        component: TelegramFilled,
+        value: props.telegram
+      },
+      {
+        component: TwitterFilled,
+        value: props.twitter
+      },
+      {
+        component: DocsFilled,
+        value: props.docs
+      }
+    ])
+
+    const clickSocialItem = (value: string) => {
+      return value ? toSocialEnd(value) : null
+    }
+
     return () => (
       <div>
-        <div class={styles.startupSocialItem}>
-          {website.value ? (
-            <WebsiteFilled
-              class={styles.activeSocialItemIcon}
-              onClick={() => toSocialEnd(website.value)}
-            />
-          ) : (
-            <WebsiteFilled class={styles.inactiveSocialItemIcon} />
-          )}
-        </div>
-        <div class={styles.startupSocialItem}>
-          {discord.value ? (
-            <DiscordFilled
-              class={styles.activeSocialItemIcon}
-              onClick={() => toSocialEnd(discord.value)}
-            />
-          ) : (
-            <DiscordFilled class={styles.inactiveSocialItemIcon} />
-          )}
-        </div>
-        <div class={styles.startupSocialItem}>
-          {telegram.value ? (
-            <TelegramFilled
-              class={styles.activeSocialItemIcon}
-              onClick={() => toSocialEnd(telegram.value)}
-            />
-          ) : (
-            <TelegramFilled class={styles.inactiveSocialItemIcon} />
-          )}
-        </div>
-        <div class={styles.startupSocialItem}>
-          {twitter.value ? (
-            <TwitterFilled
-              class={styles.activeSocialItemIcon}
-              onClick={() => toSocialEnd(twitter.value)}
-            />
-          ) : (
-            <TwitterFilled class={styles.inactiveSocialItemIcon} />
-          )}
-        </div>
-        <div class={styles.startupSocialItem}>
-          {docs.value ? (
-            <DocsFilled
-              class={styles.activeSocialItemIcon}
-              onClick={() => toSocialEnd(docs.value)}
-            />
-          ) : (
-            <DocsFilled class={styles.inactiveSocialItemIcon} />
-          )}
-        </div>
+        {socialData.value.map(socialItemData => {
+          return (
+            <div class={styles.startupSocialItem}>
+              <socialItemData.component
+                class={
+                  socialItemData.value ? styles.activeSocialItemIcon : styles.inactiveSocialItemIcon
+                }
+                onClick={() => clickSocialItem(socialItemData.value)}
+              />
+            </div>
+          )
+        })}
       </div>
     )
   }
