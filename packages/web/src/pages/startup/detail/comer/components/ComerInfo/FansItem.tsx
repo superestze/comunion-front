072d2b84baff7d8586208d2afafd7d48b7fa78c0
geekknight <geekknight@163.com>
2022-06-21 00:45:11 +0800

    fix: 🐛 fans skills style

--- packages/web/src/pages/startup/detail/comer/components/ComerInfo/FansItem.tsx
+++ packages/web/src/pages/startup/detail/comer/components/ComerInfo/FansItem.tsx
@@ -16,17 +16,18 @@ export const FansItem = defineComponent({
           src={props.fansItem?.comerProfile?.avatar || ''}
           class="rounded-1/2 h-18 w-18 mr-4"
         />
-        <div class="flex-1">
+        <div class="flex-1 min-w-0">
           <div class="u-title1">{props.fansItem?.comerProfile?.name}</div>
-          <div class="flex u-body1">
-            {props.fansItem?.comerProfile?.skills?.map((skill, skillIndex) => (
-              <div key={skill.id}>
-                <span class="u-body1">{skill.name}</span>
+          <div class="truncate u-body1">
+            {props.fansItem?.comerProfile?.skills?.map(skill => skill.name + 111777).join(' | ')}
+            {/* map((skill, skillIndex) => (
+              <span class="truncate" key={skill.id}>
+                <span class="u-body1">11111111{skill.name}</span>
                 {skillIndex + 1 !== props.fansItem?.comerProfile?.skills.length && (
                   <span class="u-grey5 mx-2">|</span>
                 )}
-              </div>
-            ))}
+              </span>
+            ))} */}
           </div>
         </div>
       </div>
