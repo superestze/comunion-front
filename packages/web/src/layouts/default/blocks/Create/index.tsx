c24dd2175841b3cc7fe8fdae578281ab4aaf7823
geekknight <geekknight@163.com>
2022-07-31 10:48:59 +0800

    feat: ✨ annotate create crowdfunding

--- packages/web/src/layouts/default/blocks/Create/index.tsx
+++ packages/web/src/layouts/default/blocks/Create/index.tsx
@@ -127,7 +127,8 @@ const CreateBlock = defineComponent({
                     </div>
                   </div>
                 </div>
-              )
+              ),
+              disabled: true
             }
           ]}
         >
