e95068e4b915a676cc0f3a9820191fbfc8f0a039
geekknight <geekknight@163.com>
2022-08-13 15:36:53 +0800

    perf: ⚡️ formatToFixed

--- packages/web/src/utils/numberFormat.ts
+++ packages/web/src/utils/numberFormat.ts
@@ -1,7 +1,21 @@
-export function formatToFloor(value: number, precision: number) {
-  return Math.floor(value * Math.pow(10, precision)) / Math.pow(10, precision)
+export function formatToFloor(value: number | string, precision: number) {
+  return String(value)
+    .toString()
+    .replace(/\.(\d+)/, (e, $1) => {
+      return `.${$1.substr(0, precision)}`
+    })
+    .replace(/(?:\.0*|(\.\d+?)0+)$/, '$1')
 }
 
-export function formatToFixed(value: number, precision: number) {
-  return Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision)
+export function formatToFixed(value: number | string, precision: number) {
+  return String(value)
+    .toString()
+    .replace(/\.(\d+)/, (e, $1) => {
+      if ($1.length > precision) {
+        return `.${Math.round(Number($1.substr(0, precision) + '.' + $1[precision + 1]))}`
+      } else {
+        return `.${$1.substr(0, precision)}`
+      }
+    })
+    .replace(/(?:\.0*|(\.\d+?)0+)$/, '$1')
 }
