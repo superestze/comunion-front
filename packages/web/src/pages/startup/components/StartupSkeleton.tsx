27d6244d7ea44c1bee2f7d7f09dbcb65503f7fa4
tower1229 <tower1229@foxmail.com>
2022-08-28 20:36:07 +0800

    fix(style): 🐛 🎨 use windi class

--- packages/web/src/pages/startup/components/StartupSkeleton.tsx
+++ packages/web/src/pages/startup/components/StartupSkeleton.tsx
@@ -5,26 +5,26 @@ const StartupSkeleton = defineComponent({
   name: 'StartupSkeleton',
   setup(props) {
     return () => (
-      <div class="bg-white rounded h-80 relative cursor-pointer">
+      <div class="bg-white rounded cursor-pointer h-80 relative">
         <div class="p-6">
-          <div class="overflow-hidden mb-2rem">
-            <USkeleton class="float-left rounded-md" width="4rem" height="4rem" />
+          <div class="mb-2rem overflow-hidden">
+            <USkeleton class="rounded-md float-left" width="4rem" height="4rem" />
             <USkeleton
-              class="float-right rounded-tr-md rounded-bl-md"
+              class="rounded-tr-md rounded-bl-md float-right"
               width="3rem"
               height="1.6rem"
             />
             <USkeleton
-              class="float-right rounded-tl-md rounded-br-md clear-right mt-0.8rem"
+              class="rounded-tl-md rounded-br-md mt-0.8rem float-right clear-right"
               width="5rem"
               height="1.6rem"
             />
           </div>
-          <USkeleton class=" mb-1rem" height="1.25rem" />
-          <USkeleton class=" mb-1rem" height="1.25rem" />
-          <USkeleton class=" mb-1rem" height="1.25rem" />
+          <USkeleton class="mb-1 h5" />
+          <USkeleton class="mb-1 h5" />
+          <USkeleton class="mb-1 h5" />
           <USkeleton
-            class="float-left rounded-tl-md rounded-br-md clear-right mt-2rem"
+            class="rounded-tl-md rounded-br-md mt-2rem float-left clear-right"
             width="5rem"
             height="1.6rem"
           />
