a489dcfb1090492f040f0c9953612e647b8d0fe2
tower1229 <tower1229@foxmail.com>
2022-08-30 14:21:46 +0800

    fix(web): 🐛 update bounty cart style

--- packages/web/src/pages/bounty/components/BountyCard.tsx
+++ packages/web/src/pages/bounty/components/BountyCard.tsx
@@ -47,7 +47,7 @@ const StartupCard = defineComponent({
 
     return () => (
       <div
-        class="h-36 cursor-pointer border-1 bg-white rounded-md px-10 mb-1.5rem pt-2rem hover:shadow-md"
+        class="bg-white rounded-md cursor-pointer border-1 h-36 mb-1.5rem px-10 pt-2rem hover:shadow-md"
         style="transition:all ease .3s"
         onClick={handleCard(props.startup.bountyId)}
       >
@@ -56,16 +56,16 @@ const StartupCard = defineComponent({
             src={props.startup.logo}
             width="10"
             height="10"
-            class="h-3.75rem w-3.75rem rounded-md mr-3"
+            class="rounded-md h-3.75rem mr-3 w-3.75rem"
           />
 
           <div class="flex-1 ">
-            <div class="flex items-center mb-2">
-              <div class="max-w-50rem text-[#333] font-700 text-1rem truncate leading-7">
+            <div class="flex mb-2 items-center">
+              <div class="font-700 max-w-50rem text-[#333] text-1rem leading-7 truncate">
                 {props.startup.title}
               </div>
               <span
-                class="inline-block px-2 h-1.25rem leading-1.25rem text-0.75rem rounded-sm ml-2"
+                class="rounded-sm h-1.25rem ml-2 px-2 text-0.75rem leading-1.25rem inline-block"
                 style={{
                   'background-color': color ? color.value : BOUNTY_TYPES_COLOR_MAP[0].value,
                   color: '#fff'
@@ -74,11 +74,11 @@ const StartupCard = defineComponent({
                 {color ? color.label : BOUNTY_TYPES_COLOR_MAP[0].label}
               </span>
             </div>
-            <div class="flex items-center flex-row">
+            <div class="flex flex-row ml-8 items-center">
               {props.startup.applicationSkills.length &&
                 props.startup.applicationSkills.map((tag: string, i: number) => {
                   return (
-                    <UTag class="px-2 mr-2 !h-1.25rem !leading-1.25rem !text-[#3F2D99] !border-[#3F2D99]">
+                    <UTag class="mr-2 px-2 !border-[#3F2D99] !h-1.25rem !text-[#3F2D99] !leading-1.25rem">
                       {tag}
                     </UTag>
                   )
@@ -106,23 +106,23 @@ const StartupCard = defineComponent({
             )
           })}
         </div>
-        <div class="flex items-center text-0.75rem ml-4.5rem">
-          <div class="flex-1 flex items-center">
-            <span class="text-[#5331F4] mr-1">
+        <div class="flex ml-4.5rem text-0.75rem items-center">
+          <div class="flex flex-1 items-center">
+            <span class="mr-1 text-[#5331F4]">
               {props.startup.paymentType === 'Stage' ? (
-                <StageOutlined class="w-1rem h-1rem" />
+                <StageOutlined class="h-1rem w-1rem" />
               ) : (
-                <CalendarOutlined class="w-1rem h-1rem" />
+                <CalendarOutlined class="h-1rem w-1rem" />
               )}
             </span>
             <span class="text-[#5331F4]">{props.startup.paymentType}</span>
-            <span class="font-700 text-[#D9D9D9] mx-2">·</span>
+            <span class="font-700 mx-2 text-[#D9D9D9]">·</span>
             <span class="text-[#9F9F9F]">{props.startup.applicantCount ?? 0} Applicant</span>
-            <span class="font-700 text-[#D9D9D9] mx-2">·</span>
+            <span class="font-700 mx-2 text-[#D9D9D9]">·</span>
             <span class="text-[#9F9F9F] ">Created {date.value}</span>
           </div>
           <span class="text-grey2">Deposit：</span>
-          <em class="text-0.875rem font-700 text-[#F29F39]">
+          <em class="font-700 text-0.875rem text-[#F29F39]">
             {props.startup.depositRequirements} USDC
           </em>
         </div>
