4946dc7b8d63c7424f6117637d3e9cc30e459178
tower1229 <tower1229@foxmail.com>
2022-08-31 09:28:30 +0800

    fix(style): 🐛 🎨 create bounty form bug

--- packages/web/src/blocks/Bounty/components/PayDetailStage.tsx
+++ packages/web/src/blocks/Bounty/components/PayDetailStage.tsx
@@ -67,7 +67,7 @@ const PayDetailStage = defineComponent({
       >
         {/* <UFormItemsFactory fields={this.payDetailStageFields} values={this.bountyInfo} /> */}
         {this.bountyInfo.stages.map((stage: any, stageIndex: number) => (
-          <div class="mb-6 flex items-center">
+          <div class="flex mb-6 items-center">
             <div class="bg-purple rounded-lg px-4 pt-4">
               <div class="text-grey1">Rewards</div>
               {/* <div class="flex items-center"> */}
@@ -88,7 +88,7 @@ const PayDetailStage = defineComponent({
                   v-model:value={stage.token1Amount}
                   renderUnit={() => renderUnit(this.bountyInfo.token1Symbol)}
                 ></UInputNumberGroup>
-                <div class="text-grey2 text-3xl px-5">+</div>
+                <div class="px-5 text-grey2 text-3xl">+</div>
                 <UInputNumberGroup
                   class="flex-1"
                   type="withUnit"
@@ -108,10 +108,10 @@ const PayDetailStage = defineComponent({
               </div>
               <div class="grid grid-cols-[1fr,56px,1fr]">
                 {!this.bountyInfo.token2Symbol && (
-                  <div class="text-grey4 text-xs col-start-3 mt-2">
+                  <div class="mt-2 text-xs text-grey4 col-start-3">
                     Not setup token yet, go to{' '}
                     <span
-                      class="text-primary cursor-pointer"
+                      class="cursor-pointer text-primary"
                       onClick={() => this.showLeaveTipModal()}
                     >
                       Finance Setting
@@ -128,7 +128,7 @@ const PayDetailStage = defineComponent({
                     trigger: 'blur'
                   },
                   {
-                    validator: (rule, value) => value.length > 12,
+                    validator: (rule, value) => !value || value.length > 12,
                     message: 'Stage decription must be 12 characters or more',
                     trigger: 'blur'
                   }
@@ -155,7 +155,7 @@ const PayDetailStage = defineComponent({
               ]}
               onClick={() => (this.bountyInfo.stages.length > 1 ? this.delStage(stageIndex) : null)}
             >
-              <MinusCircleOutlined class="w-5 h-5 ml-5" />
+              <MinusCircleOutlined class="h-5 ml-5 w-5" />
             </div>
 
             <div
@@ -168,11 +168,11 @@ const PayDetailStage = defineComponent({
               ]}
               onClick={stageIndex + 1 === this.bountyInfo.stages.length ? this.addStage : undefined}
             >
-              <AddCircleOutlined class="w-5 h-5 ml-5" />
+              <AddCircleOutlined class="h-5 ml-5 w-5" />
             </div>
           </div>
         ))}
-        <div class="bg-purple py-5.5 px-6 mr-20 rounded-lg">
+        <div class="bg-purple rounded-lg mr-20 py-5.5 px-6">
           The current total rewards as{' '}
           <span class="text-primary">
             <span class={[{ 'text-error': this.payStagesTotal.usdcTotal > MAX_AMOUNT }]}>
