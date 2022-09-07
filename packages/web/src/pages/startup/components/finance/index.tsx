50b3d8f3628d0ef0e20bddd7a054357d4d90f50c
tower1229 <tower1229@foxmail.com>
2022-09-07 15:47:21 +0800

    fix(style): 🐛 🎨 fix ui bugs

--- packages/web/src/pages/startup/components/finance/index.tsx
+++ packages/web/src/pages/startup/components/finance/index.tsx
@@ -74,12 +74,12 @@ export default defineComponent({
   render() {
     return (
       <UCard title="FINANCE" class="mb-6">
-        <div class="flex flex-col mr-10">
+        <div class="flex flex-col">
           {this.financeBasic.map(item => {
             return (
               <div class="flex mt-4">
                 <p class="text-grey3 w-38 u-body2">{item.name}</p>
-                <p class="text-primary2 u-body2">{item.value}</p>
+                <p class="text-primary2 overflow-hidden u-body2">{item.value}</p>
               </div>
             )
           })}
@@ -87,13 +87,9 @@ export default defineComponent({
           <div class="bg-grey5 h-1px mt-6.5 w-full"></div>
           <div class="flex mt-5.5">
             <p class="text-grey3 w-38 u-body2">Presale wallet：</p>
-            <div class="flex flex-col">
+            <div class="overflow-hidden">
               {(this.wallets || []).map(item => {
-                return (
-                  <p>
-                    <UAddress class="text-primary u-body2" autoSlice address={item.value} />
-                  </p>
-                )
+                return <UAddress class="text-primary u-body2" autoSlice address={item.value} />
               })}
             </div>
           </div>
