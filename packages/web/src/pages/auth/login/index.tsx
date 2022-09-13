ee86695d52b8133bc24d380d254d724586b1ea53
tower1229 <tower1229@foxmail.com>
2022-09-13 16:03:37 +0800

    fix(web): 🐛 login page style

--- packages/web/src/pages/auth/login/index.tsx
+++ packages/web/src/pages/auth/login/index.tsx
@@ -53,8 +53,8 @@ export default defineComponent({
       <>
         <div class="flex bg-[#EDEDF2] h-100vh w-100vw justify-center items-center">
           <MoreNavigationPage />
-          <div class="bg-white rounded-8px h-542px max-w-1245px relative">
-            <div class="bg-primary flex flex-col rounded-2px h-606px text-white pt-162px pl-64px -top-32px left-100px w-635px absolute">
+          <div class="bg-white flex rounded-8px flex-1 h-542px max-w-1245px">
+            <div class="bg-primary rounded-2px h-606px -mt-8 text-white ml-25 pt-162px pl-64px w-635px ">
               <p style={{ color: 'rgba(255,255,255,0.8)' }}>Welcome to Comunion</p>
               <p class="font-bold mt-30px text-30px">The First</p>
               <p class="font-bold mt-16px text-30px">Permissionless Economic Network </p>
@@ -62,45 +62,47 @@ export default defineComponent({
                 <ULogo height={20} withText theme="white" />
               </div>
             </div>
-            <div class="flex flex-col mr-101px w-308px">
-              <div class="mx-auto h-48px mt-60px mb-20px w-48px">
-                <img
-                  src={logo}
-                  srcset={`${logo}, ${logo2} 2x, ${logo3} 3x`}
-                  alt="logo"
-                  class="w-full"
-                />
-              </div>
-              <p class="font-bold text-center text-[#111111] text-28px">Comunion</p>
-              <p
-                class="mt-54px text-center mb-20px"
-                style={{
-                  color: 'rgba(17, 17, 17, 0.6)'
-                }}
-              >
-                WALLET
-              </p>
-              <UButton
-                class="mx-auto h-40px text-white text-center w-full text-16px relative"
-                size="small"
-                type="primary"
-                loading={this.loading}
-                onClick={walletLogin}
-              >
-                <WalletOutlined class="h-20px top-10px left-17px w-20px absolute" />
-                Connect to a wallet
-              </UButton>
-              <div class="flex mt-10 mb-24px items-center">
-                <div class="bg-[#666] h-[1px] w-[140px]" />
-                <div
-                  class="text-center text-[14px] leading-5 w-[40px]"
-                  style={{ color: 'rgba(17,17,17,0.6)' }}
-                >
-                  OR
+            <div class="flex-1">
+              <div class="mx-auto w-308px">
+                <div class="mx-auto h-48px mt-60px mb-20px w-48px">
+                  <img
+                    src={logo}
+                    srcset={`${logo}, ${logo2} 2x, ${logo3} 3x`}
+                    alt="logo"
+                    class="w-full"
+                  />
                 </div>
-                <div class="bg-[#666] h-[1px] w-[140px]" />
+                <p class="font-bold text-center text-[#111111] text-28px">Comunion</p>
+                <p
+                  class="mt-54px text-center mb-20px"
+                  style={{
+                    color: 'rgba(17, 17, 17, 0.6)'
+                  }}
+                >
+                  WALLET
+                </p>
+                <UButton
+                  class="mx-auto h-40px text-white text-center w-full text-16px relative"
+                  size="small"
+                  type="primary"
+                  loading={this.loading}
+                  onClick={walletLogin}
+                >
+                  <WalletOutlined class="h-20px top-10px left-17px w-20px absolute" />
+                  Connect to a wallet
+                </UButton>
+                <div class="flex mt-10 mb-24px items-center">
+                  <div class="bg-[#666] h-[1px] w-[140px]" />
+                  <div
+                    class="text-center text-[14px] leading-5 w-[40px]"
+                    style={{ color: 'rgba(17,17,17,0.6)' }}
+                  >
+                    OR
+                  </div>
+                  <div class="bg-[#666] h-[1px] w-[140px]" />
+                </div>
+                <OAuthSignWidget />
               </div>
-              <OAuthSignWidget />
             </div>
           </div>
         </div>
