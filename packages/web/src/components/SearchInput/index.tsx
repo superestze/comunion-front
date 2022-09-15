9c10c49089e848d50b790272c6dc4e4f4b84d3e3
tower1229 <tower1229@foxmail.com>
2022-09-15 22:18:23 +0800

    feat(web): ✨ global bg is white

--- packages/web/src/components/SearchInput/index.tsx
+++ packages/web/src/components/SearchInput/index.tsx
@@ -63,7 +63,7 @@ const SearchInput = defineComponent({
             }, 300)
           }}
         >
-          <div class={`bg-white w-full h-full rounded-sm px-3 flex items-center`}>
+          <div class={`w-full h-full rounded-sm px-3 flex items-center`}>
             <span class="truncate">{this.input || 'Search'}</span>
           </div>
           <NInput
