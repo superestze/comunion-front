3470491b3619827190cb1a93532d132f1bb746e2
geekknight <geekknight@163.com>
2022-09-15 23:35:51 +0800

    fix: 🐛 bugs

--- packages/web/src/pages/startup/setting/components/governance/typing.d.ts
+++ packages/web/src/pages/startup/setting/components/governance/typing.d.ts
@@ -2,7 +2,9 @@ export interface StrategyType {
   dictValue?: string
   dictLabel?: string
   network?: number
+  chainId?: number
   voteDecimals?: number
   contractAddress?: string
+  tokenContractAddress?: string
   voteSymbol?: string
 }
