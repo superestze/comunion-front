2d907c13c35860257262ac6ea0f512df7cca1d43
tower1229 <tower1229@foxmail.com>
2022-09-17 13:38:35 +0800

    feat(web): ✨ bug fix

--- packages/web/src/pages/comer/components/filter/index.tsx
+++ packages/web/src/pages/comer/components/filter/index.tsx
@@ -1,6 +1,7 @@
 import { UTabPane, UTabs } from '@comunion/components'
 import { defineComponent, PropType, ref } from 'vue'
 import { ModuleTags } from '@/components/Tags'
+import './style.css'
 
 export default defineComponent({
   name: 'Filter',
@@ -15,15 +16,19 @@ export default defineComponent({
   setup() {
     const tag1 = ref<any>()
     const tag2 = ref<any>()
+    const currentTab = ref<string>('CREATED')
+
     return {
       tag1,
-      tag2
+      tag2,
+      currentTab
     }
   },
   render() {
     const tabsChange = (value: string) => {
       if (this.tag1) this.tag1.selectedList = ['All']
       if (this.tag2) this.tag2.selectedList = ['All']
+      this.currentTab = value
       this.$emit('selectedTagChange', ['All'])
       this.$emit('tabChange', value === 'CREATED')
     }
@@ -34,8 +39,20 @@ export default defineComponent({
 
     return (
       <div class="bg-white border border-color-border rounded-sm mb-6 py-6 px-8 relative overflow-hidden">
-        <UTabs onUpdateValue={tabsChange}>
-          <UTabPane name="CREATED" tab="Created">
+        <UTabs class="dashboard-tabs" onUpdateValue={tabsChange}>
+          <UTabPane
+            name="CREATED"
+            tab="Created"
+            v-slots={{
+              tab: () => {
+                return (
+                  <div class={`${this.currentTab === 'CREATED' ? 'text-color1' : 'text-color3'}`}>
+                    Created
+                  </div>
+                )
+              }
+            }}
+          >
             <ModuleTags
               class="mt-2"
               tasks={this.tasks}
@@ -43,7 +60,21 @@ export default defineComponent({
               ref={(ref: any) => (this.tag2 = ref)}
             />
           </UTabPane>
-          <UTabPane name="PARTICIPATED" tab="Participated">
+          <UTabPane
+            name="PARTICIPATED"
+            tab="Participated"
+            v-slots={{
+              tab: () => {
+                return (
+                  <div
+                    class={`${this.currentTab === 'PARTICIPATED' ? 'text-color1' : 'text-color3'}`}
+                  >
+                    Participated
+                  </div>
+                )
+              }
+            }}
+          >
             <ModuleTags
               class="mt-2"
               tasks={this.tasks}
