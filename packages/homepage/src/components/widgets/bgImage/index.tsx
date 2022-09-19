89ba02aad7111618802cc3f53301d384af208c69
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-09-19 20:03:55 +0800

    fix(homepage): 🐛 fix index media

--- packages/homepage/src/components/widgets/bgImage/index.tsx
+++ packages/homepage/src/components/widgets/bgImage/index.tsx
@@ -54,19 +54,19 @@ export default defineComponent({
         <div ref="divRef" class={[styles.bgImage]}>
           <div class="absolute left-0 right-0 top-0 bottom-0 m-auto w-256 h-35 <md:w-[90vw]">
             <div class="font-primary font-semibold">
-              <p class="text-[rgba(255,255,255,0.5)] text-[20px] mb-3 absolute left-[2.1rem] top-[-1.5rem] <md:left-[0] <md:top-[-3rem]">
+              <p class="text-[rgba(255,255,255,0.5)] text-[20px] mb-3 absolute left-[2.1rem] top-[-1.5rem] <md:left-[3rem] <md:top-[-3rem] <md:text-[1vw]">
                 The First
               </p>
             </div>
-            <p class="font-medium text-[#FFF1F1]">
-              <span class="ml-[1.9rem] text-[64px] <md:ml-[0] <md:text-[8vw]">
+            <p class="font-medium text-[#FFF1F1] <md:pl-[3rem]">
+              <span class="ml-[1.9rem] text-[64px] <md:ml-[0] <md:text-[8vw] <md:font-700 <md:font-primary <md:font-medium">
                 Permissionless {this.text}
               </span>
               <span
                 class={`${styles.lineColor} line-color inline-block h-13 w-1 bg-[#ffffff] ml-2`}
               ></span>
             </p>
-            <p class="text-[#FFFFFF] text-[32px] mt-8.5 ml-[13rem] <md:ml-[0] <md:text-[4.7vw]">
+            <p class="text-[#FFFFFF] text-[32px] mt-8.5 ml-[13rem] <md:ml-[3rem] <md:text-[3.5vw] <md:font-700">
               to narrow the gap between rich and poor
             </p>
           </div>
