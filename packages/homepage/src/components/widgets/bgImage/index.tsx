0f5c7721ad8deb387cb5880671f25c8cdba513c6
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-09-19 14:46:38 +0800

    feat(web): ✨ style index

--- packages/homepage/src/components/widgets/bgImage/index.tsx
+++ packages/homepage/src/components/widgets/bgImage/index.tsx
@@ -23,13 +23,8 @@ export default defineComponent({
       nextTick(() => {
         const distance = divRef.value.scrollHeight
         window.scrollTo({
-          //滚动到元素位置
-          //top: this.$refs[元素].offsetTop,//offsetTop 相对于offsetparent 定位,当有定位时，不准确。
-          top: distance, //推荐使用，getBoundingClientRect 相对于当前视口的位置
-          //top: 400//滚动到指定距离
-          //top: 0,//滚动到顶部
-          //top: document.documentElement.scrollHeight,//滚动到底部
-          behavior: 'smooth' // 平滑滚动
+          top: distance,
+          behavior: 'smooth'
         })
       })
     }
