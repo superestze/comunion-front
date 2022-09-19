a4047c8174cee20b265ced185c69c0a52e25ab2f
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-09-19 17:09:27 +0800

    fix(homepage): 🐛 style

--- packages/homepage/src/components/widgets/bgImage/index.tsx
+++ packages/homepage/src/components/widgets/bgImage/index.tsx
@@ -42,18 +42,22 @@ export default defineComponent({
     return (
       <>
         <div ref="divRef" class={`${styles.bgImage} overflow-hidden`}>
-          <div class="font-primary font-semibold pt-66.75 pl-[16vw]">
-            <p class="text-[rgba(255,255,255,0.5)] text-[20px] mb-3">The First</p>
+          <div class="absolute left-0 right-0 top-0 bottom-0 m-auto w-256 h-35">
+            <div class="font-primary font-semibold">
+              <p class="text-[rgba(255,255,255,0.5)] text-[20px] mb-3 absolute left-[2.1rem] top-[-1.5rem]">
+                The First
+              </p>
+            </div>
+            <p class="font-medium text-[#FFF1F1] text-[64px] text-center">
+              <span>Permissionless {this.text}</span>
+              {this.lineStatus && (
+                <span class="inline-block h-13 w-1 bg-[#ffffff] ml-2 animate-pulse"></span>
+              )}
+            </p>
+            <p class="text-[#FFFFFF] text-[32px] text-center mt-8.5">
+              to narrow the gap between rich and poor
+            </p>
           </div>
-          <p class="font-medium text-[#FFF1F1] text-[64px] text-center">
-            <span>Permissionless {this.text}</span>
-            {this.lineStatus && (
-              <span class="inline-block h-13 w-1 bg-[#ffffff] ml-2 animate-pulse"></span>
-            )}
-          </p>
-          <p class="text-[#FFFFFF] text-[32px] text-center mt-8.5">
-            to narrow the gap between rich and poor
-          </p>
           <img
             onClick={this.onScroll}
             class="absolute bottom-15 left-0 right-0 mx-auto w-4 h-4 cursor-pointer animate-bounce"
