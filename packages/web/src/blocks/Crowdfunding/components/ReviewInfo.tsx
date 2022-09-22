229baa27e86e83f4571f49484fd8e899a002f734
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-09-22 19:55:50 +0800

    fix(web): 🐛 fix bug

--- packages/web/src/blocks/Crowdfunding/components/ReviewInfo.tsx
+++ packages/web/src/blocks/Crowdfunding/components/ReviewInfo.tsx
@@ -15,67 +15,67 @@ export const ReviewInfo = defineComponent({
   render() {
     return (
       <div class="review-info">
-        <div class="review-info-item">
-          <div class="title">Startup:</div>
+        <div class="review-info-item u-h6">
+          <div class="title w-[220px]">Startup:</div>
           <div>{this.crowdfundingInfo.startupName}</div>
         </div>
         <div class="review-info-item">
-          <div class="title">Token Contract :</div>
-          <div>{this.crowdfundingInfo.sellTokenContract}</div>
+          <div class="title w-[220px]">Token Contract :</div>
+          <div class="text-primary">{this.crowdfundingInfo.sellTokenContract}</div>
         </div>
         <div class="review-info-item">
-          <div class="title">Team Wallet Address :</div>
-          <div>{this.crowdfundingInfo.teamWallet}</div>
+          <div class="title w-[220px]">Team Wallet Address :</div>
+          <div class="text-primary">{this.crowdfundingInfo.teamWallet}</div>
         </div>
         <div class="review-info-item">
-          <div class="title">Raise Goal :</div>
+          <div class="title w-[220px]">Raise Goal :</div>
           <div>
             {this.crowdfundingInfo.raiseGoal} {this.crowdfundingInfo.buyTokenName}
           </div>
         </div>
         <div class="review-info-item">
-          <div class="title">Tokens For dCrowdfunding :</div>
+          <div class="title w-[220px]">Tokens For dCrowdfunding :</div>
           <div>
             {this.crowdfundingInfo.buyPrice! * this.crowdfundingInfo.raiseGoal!}{' '}
             {this.crowdfundingInfo.sellTokenName}
           </div>
         </div>
         <div class="review-info-item">
-          <div class="title">Swap :</div>
+          <div class="title w-[220px]">Swap :</div>
           <div>{this.crowdfundingInfo.swapPercent}%</div>
         </div>
         <div class="review-info-item">
-          <div class="title">IBO Rate :</div>
+          <div class="title w-[220px]">Rate :</div>
           <div>
             1 {this.crowdfundingInfo.buyTokenName} = {this.crowdfundingInfo.buyPrice}{' '}
             {this.crowdfundingInfo.sellTokenName}
           </div>
         </div>
         <div class="review-info-item">
-          <div class="title">Maximum Buy :</div>
+          <div class="title w-[220px]">Maximum Buy :</div>
           <div>
             {this.crowdfundingInfo.maxBuyAmount} {this.crowdfundingInfo.buyTokenName}
           </div>
         </div>
         <div class="review-info-item">
-          <div class="title">Sell Tax :</div>
+          <div class="title w-[220px]">Sell Tax :</div>
           <div>{this.crowdfundingInfo.sellTax} %</div>
         </div>
         <div class="review-info-item">
-          <div class="title">Maximum Sell :</div>
+          <div class="title w-[220px]">Maximum Sell :</div>
           <div>{this.crowdfundingInfo.maxSell} % of the bought token amount</div>
         </div>
         <div class="review-info-item">
-          <div class="title">dCrowdfunding Start Time :</div>
+          <div class="title w-[220px]">dCrowdfunding Start Time :</div>
           <div>{dayjs(this.crowdfundingInfo.startTime).format('YYYY-MM-DD HH:mm')}</div>
         </div>
         <div class="review-info-item">
-          <div class="title">dCrowdfunding End Time :</div>
+          <div class="title w-[220px]">dCrowdfunding End Time :</div>
           <div>{dayjs(this.crowdfundingInfo.endTime).format('YYYY-MM-DD HH:mm')}</div>
         </div>
         {!!this.crowdfundingInfo.youtube && (
           <div class="review-info-item">
-            <div class="title">Youtube :</div>
+            <div class="title w-[220px]">Youtube :</div>
             <a href={this.crowdfundingInfo.youtube} target="__blank" class="text-primary">
               {this.crowdfundingInfo.youtube}
             </a>
@@ -83,7 +83,7 @@ export const ReviewInfo = defineComponent({
         )}
         {!!this.crowdfundingInfo.detail && (
           <div class="review-info-item">
-            <div class="title">dCrowdfunding detail :</div>
+            <div class="title w-[220px]">dCrowdfunding detail :</div>
 
             <a href={this.crowdfundingInfo.detail} target="__blank" class="text-primary">
               {this.crowdfundingInfo.detail}
