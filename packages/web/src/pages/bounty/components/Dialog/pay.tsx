512168cce6f04155484b962313bc865e7eaef319
lishiyao <40553790+nextniko@users.noreply.github.com>
2022-10-01 20:19:51 +0800

    fix(web): 🐛 bounty detail pay fix

--- packages/web/src/pages/bounty/components/Dialog/pay.tsx
+++ packages/web/src/pages/bounty/components/Dialog/pay.tsx
@@ -76,11 +76,6 @@ export default defineComponent({
           // required: true,
           rules: [
             {
-              required: true,
-              message: 'Pay amount cannot be blank',
-              trigger: 'blur'
-            },
-            {
               validator: (rule, value) => {
                 return value > 0 || (formData.token2Amount > 0 && value >= 0)
               },
