401c9aa7760d0a1447861b5f6e94d96e011e24be
tower1229 <tower1229@foxmail.com>
2022-10-04 21:48:48 +0800

    fix(web): 🐛 root route use default layout

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -1,5 +1,5 @@
 export const layoutGroupedRoutes = {
-  'empty/index': ['/', '/ipfs/raw/*'],
+  'empty/index': ['/ipfs/raw/*'],
   'auth/index': ['/auth/login', '/auth/callback/*', '/auth/register/*', '/auth/association']
   // default: ['*']
 } as const
